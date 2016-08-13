716dc0c3319b7b97b6541c9f458ee91ff8ffa3ab
mrchebik <mrchebik@yandex.ru>
2016-08-13 13:32:27 +0300

    migrate 'AddNoteController' and 'ViewNotesController' to the 'NotesController'

--- src/main/java/ru/mrchebik/web/NotesController.java
+++ src/main/java/ru/mrchebik/web/NotesController.java
@@ -1,9 +1,19 @@
 package ru.mrchebik.web;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import ru.mrchebik.data.NoteRepository;
+import ru.mrchebik.entity.Note;
+import ru.mrchebik.session.UserSession;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import static org.springframework.web.bind.annotation.RequestMethod.GET;
+import static org.springframework.web.bind.annotation.RequestMethod.POST;
 
 /**
  * Created by mrchebik on 08.08.16.
@@ -11,10 +21,57 @@ import static org.springframework.web.bind.annotation.RequestMethod.GET;
 @Controller
 @RequestMapping("/notes")
 public class NotesController {
+    private NoteRepository noteRepository;
+
+    public NotesController() {
+
+    }
+
+    @Autowired
+    public NotesController(NoteRepository noteRepository) {
+        this.noteRepository = noteRepository;
+    }
 
     @RequestMapping(method = GET)
     public String notes() {
         return "Notes";
     }
 
+    @RequestMapping(value = "/add", method = GET)
+    public String Get() {
+        return "AddNote";
+    }
+
+    @RequestMapping(value = "/add", method = POST)
+    public String add(@RequestParam String title,
+                      @RequestParam String text) {
+        try {
+            noteRepository.add(new Note(UserSession.getId(), title, text));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return "AddNote";
+    }
+
+    @RequestMapping(value = "/view", method = GET)
+    public String view(@RequestParam(value = "hide", defaultValue = "1") int page,
+                       Model model) {
+
+        List<Note> notes = new ArrayList<>(noteRepository.findNotes(UserSession.getId()));
+        UserSession.setPages(notes, UserSession.getCount());
+
+        if (notes.size() > UserSession.getCount()) {
+            if (page * UserSession.getCount() > notes.size()) {
+                notes = notes.subList((page - 1) * UserSession.getCount(), notes.size());
+            } else {
+                notes = notes.subList((page - 1) * UserSession.getCount(), page * UserSession.getCount());
+            }
+        }
+
+        model.addAttribute("notes", notes);
+        model.addAttribute("page", page);
+        model.addAttribute("pages", UserSession.getPages());
+
+        return "ViewNotes";
+    }
 }
--- src/test/java/ru/mrchebik/web/NotesControllerTest.java
+++ src/test/java/ru/mrchebik/web/NotesControllerTest.java
@@ -1,8 +1,18 @@
 package ru.mrchebik.web;
 
+import org.junit.Before;
 import org.junit.Test;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import ru.mrchebik.data.NoteRepository;
+import ru.mrchebik.entity.Note;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
 import static org.springframework.test.web.servlet.setup.MockMvcBuilders.standaloneSetup;
@@ -11,11 +21,64 @@ import static org.springframework.test.web.servlet.setup.MockMvcBuilders.standal
  * Created by mrchebik on 09.08.16.
  */
 public class NotesControllerTest {
+    private List<Note> expectedNotes;
+    private NoteRepository mockRepository;
+    private NotesController controllerDefault;
+    private NotesController controller;
+    private MockMvc mockMvcDefault;
+    private MockMvc mockMvc;
+
+    @Before
+    public void before() {
+        expectedNotes = new ArrayList<>();
+        mockRepository = mock(NoteRepository.class);
+        controllerDefault = new NotesController();
+        controller = new NotesController(mockRepository);
+        mockMvcDefault = standaloneSetup(controllerDefault).build();
+        mockMvc = standaloneSetup(controller).build();
+    }
+
     @Test
     public void testNotesPage() throws Exception {
-        NotesController controller = new NotesController();
-        MockMvc mockMvc = standaloneSetup(controller).build();
+        mockMvcDefault.perform(get("/notes")).andExpect(view().name("Notes"));
+    }
 
-        mockMvc.perform(get("/notes")).andExpect(view().name("Notes"));
+    @Test
+    public void testAddPage() throws Exception {
+        expectedNotes.add(new Note("test", "test"));
+        when(mockRepository.add(new Note("test", "test"))).thenReturn(expectedNotes.get(0));
+
+        mockMvc.perform(get("/notes/add")).andExpect(view().name("AddNote"));
+
+        expectedNotes.clear();
+    }
+
+    @Test
+    public void testViewPage() throws Exception {
+        for (int i = 0; i < 20; i++) {
+            expectedNotes.add(0, new Note("test", "test"));
+        }
+
+        when(mockRepository.findNotes(0)).thenReturn(expectedNotes);
+
+        if (expectedNotes.size() > 10) {
+            if (1 * 10 > expectedNotes.size()) {
+                expectedNotes = expectedNotes.subList(0, expectedNotes.size());
+            } else {
+                expectedNotes = expectedNotes.subList(0, 10);
+            }
+        }
+        assertEquals(expectedNotes.size(), 10);
+
+        mockMvc.perform(get("/notes/view"))
+                .andExpect(MockMvcResultMatchers.view().name("ViewNotes"))
+                .andExpect(MockMvcResultMatchers.model().attributeExists("notes"))
+                .andExpect(MockMvcResultMatchers.model().attribute("notes", expectedNotes))
+                .andExpect(MockMvcResultMatchers.model().attributeExists("page"))
+                .andExpect(MockMvcResultMatchers.model().attribute("page", 1))
+                .andExpect(MockMvcResultMatchers.model().attributeExists("pages"))
+                .andExpect(MockMvcResultMatchers.model().attribute("pages", 2));
+
+        expectedNotes.clear();
     }
 }
